550970df972f75f3a5ba7a9105914248d65f0b7f
Alexander Hortua <alexander.hortua10@gmail.com>
2022-09-30 13:00:21 -0700

    Including a Vector 3D class and the test for this class

---
+++ src/project01/Vector3D.java
@@ -0,0 +1,172 @@
+package project01;
+
+import java.util.Vector;
+
+import static java.lang.Math.*;
+
+/**
+ * This class represent a Vector3D. It has an x y and z components.
+ */
+public class Vector3D {
+
+    private double x , y , z;
+
+    /**
+     * Constructor of Vector3D class. It expects 3 components, x y , z.
+     * @param x represent the x point of the vector
+     * @param y represent the y point of the vector
+     * @param z represent the z point of the vector
+     */
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Get the x component of this class
+     * @return x
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * Get the y component of this class
+     * @return y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * Get the z component of this class;
+     * @return z
+     */
+    public double getZ() {
+        return z;
+    }
+
+
+    /**
+     * returns the magnitude of this Vector
+     * @return magnitude - The square root of the sum of each component multiplied by itself.
+     */
+    public double getMagnitude() {
+
+        double powX = pow(this.x, 2);
+        double powY = pow(this.y, 2);
+        double powZ = pow(this.z, 2);
+        double magnitude = sqrt(powX + powY + powZ);
+        return magnitude;
+    }
+
+    /**
+     * Returns a new vector that is obtained by dividing each component of a vector by its magnitude.
+     * @return a normalized Vector with the magnitude equal to one
+     * @throws IllegalStateException if the magnitude of this vector is equal to zero;
+     */
+    public Vector3D normalize() throws IllegalStateException {
+
+        if(this.getMagnitude() == 0) {
+            throw new IllegalStateException();
+        }
+
+        double normalizedX = this.getX() / this.getMagnitude();
+        double normalizedY = this.getY() / this.getMagnitude();
+        double normalizedZ = this.getY() / this.getMagnitude();
+
+        Vector3D newNormalizedVector = new Vector3D(normalizedX, normalizedY, normalizedZ);
+
+        return newNormalizedVector;
+    }
+
+    /**
+     * Returns a Vector3D that is obtained by adding respective components of this Vector3D
+     * + another vector.
+     * @param otherVector - Another Vector3D to be computed.
+     * @return a new Vector3D with the sum of the other Vectors
+     */
+    public Vector3D add (Vector3D otherVector) {
+
+        //Preparing the expected values
+        double addedX = this.getX() + otherVector.getX();
+        double addedY = this.getY() + otherVector.getY();
+        double addedZ = this.getZ() + otherVector.getZ();
+
+        return new Vector3D(addedX, addedY ,addedZ);
+    }
+
+    /**
+     * Returns a vector obtained by multiplying each component of the current vector by the provided constant.
+     * @param constantMultiplier
+     * @return a new Vector3D with each component multiplied by a constant
+     */
+    public Vector3D multiply (double constantMultiplier) {
+
+        //Preparing the expected values
+        double expectedX = this.getX() * constantMultiplier;
+        double expectedY = this.getY() * constantMultiplier;
+        double expectedZ = this.getZ() * constantMultiplier;
+
+        return new Vector3D(expectedX,expectedY,expectedZ);
+    }
+
+    /**
+     * Returns the dot product of this vector and another vector. It should not change the two vectors.
+     * @param otherVector
+     * @return the dot product of 2 vectors
+     */
+    public double dotProduct (Vector3D otherVector) {
+        double expectedDot = this.getX() * otherVector.getX() +
+                this.getY() * otherVector.getY() +
+                this.getZ() * otherVector.getZ();
+        return expectedDot;
+    }
+
+    /**
+     * Returns the angle between two vectors (this vector and other Vector) in degrees.
+     * It should not change the two vectors. This is calculated used the dot product / multiplication of magnitudes
+     * of both vectors.
+     * @param otherVector
+     * @return The angle of 2 vectors in degrees.
+     * @throws IllegalStateException if the magnitude of either vector is equal to zero;
+     */
+    public double angleBetween(Vector3D otherVector) throws IllegalStateException {
+
+        if (this.getMagnitude() == 0 || otherVector.getMagnitude() == 0) {
+            throw new IllegalStateException();
+        }
+        double radiant = acos(this.dotProduct(otherVector) / (this.getMagnitude() * otherVector.getMagnitude()));
+        return toDegrees(radiant);
+
+    };
+
+    /**
+     * Returns the cross product of this vector and another vector.
+     * It should not change the two vectors.
+     * @param otherVector
+     * @return
+     */
+    public Vector3D crossProduct (Vector3D otherVector) {
+
+        double crossX = this.getY() * otherVector.getZ() - this.getZ() * otherVector.getY();
+        double crossY = this.getZ() * otherVector.getX() - this.getX() * otherVector.getZ();
+        double crossZ = this.getX() * otherVector.getY() - this.getY() * otherVector.getX();
+
+        return new Vector3D(crossX,crossY,crossZ);
+    }
+
+    /**
+     * Override the original method and returns the components in order (x, y ,z);
+     * @return (x, y ,z);
+     */
+    @Override
+    public String toString() {
+        return "(" + String.format("%.2f",x) +
+                ", " + String.format("%.2f",y) +
+                ", " + String.format("%.2f",z) +
+                ')';
+    }
+
+}
---
+++ test/Vector3DTest.java
@@ -0,0 +1,215 @@
+import project01.Vector3D;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is the class test for the Vector3D class.
+ */
+public class Vector3DTest {
+    private Vector3D vector1, vector2, otherVector, zeroVector, negativeVector1;
+
+    /**
+     * Setting up some objects of type Vector3D.
+     */
+    @org.junit.Before
+    public void setUp() throws Exception {
+        this.vector1 = new Vector3D(2.0001,4.0001,5.0009);
+        this.vector2 = new Vector3D(-1.45f,7.25f,5);
+        this.negativeVector1 = new Vector3D(-1.56f,-12,-5.28f);
+        this.otherVector = new Vector3D(-6.54f,4.34f,-4.28f);
+        this.zeroVector = new Vector3D(0,0,0);
+    }
+
+    /**
+     * Instance of Vector3D to test the get X method
+     */
+    @org.junit.Test
+    public void getX() {
+        assertEquals(2, vector1.getX(), 0.001);
+        assertEquals(-1.45f, vector2.getX(), 0.001);
+        assertEquals(-1.56f, negativeVector1.getX(), 0.001);
+        assertEquals(-6.54f, otherVector.getX(), 0.001);
+        assertEquals(0, zeroVector.getX(), 0.001);
+    }
+
+    /**
+     * Instance of Vector3D to test the get Y method
+     */
+    @org.junit.Test
+    public void getY() {
+        assertEquals(4, vector1.getY(), 0.001);
+        assertEquals(7.25f, vector2.getY(), 0.001);
+        assertEquals(-12, negativeVector1.getY(), 0.001);
+        assertEquals(4.34f, otherVector.getY(), 0.001);
+        assertEquals(0, zeroVector.getY(), 0.001);
+    }
+
+    /**
+     * Instance of Vector3D to test the get Z method
+     */
+    @org.junit.Test
+    public void getZ() {
+        assertEquals(5, vector1.getZ(), 0.001);
+        assertEquals(5, vector2.getZ(), 0.001);
+        assertEquals(-5.28f, negativeVector1.getZ(), 0.001);
+        assertEquals(-4.28f, otherVector.getZ(), 0.001);
+        assertEquals(0, zeroVector.getZ(), 0.001);
+    }
+
+    /**
+     * Computing the magnitude of each initial object to verify getMagnitude is working
+     */
+    @org.junit.Test
+    public void getMagnitude() {
+        assertEquals(6.7082, vector1.getMagnitude(), 0.001);
+        assertEquals(8.9255, vector2.getMagnitude(), 0.001);
+        assertEquals(13.2027, negativeVector1.getMagnitude(), 0.001);
+        assertEquals(8.9401, otherVector.getMagnitude(), 0.001);
+        assertEquals(0, zeroVector.getMagnitude(), 0.001);
+    }
+
+    /**
+     * Creating an instance of Vector3D from the normalize method.
+     * And testing this new Instance with the expected values.
+     */
+    @org.junit.Test
+    public void normalize() {
+        //Vector 1 normalized expected Value
+        assertEquals(0.2981, vector1.normalize().getX(), 0.001);
+        assertEquals(0.5962, vector1.normalize().getY(), 0.001);
+        assertEquals(0.5962, vector1.normalize().getZ(), 0.001);
+
+        // Negative Vector normalized expected value
+        assertEquals(-0.1182, negativeVector1.normalize().getX(), 0.001);
+        assertEquals(-0.9089, negativeVector1.normalize().getY(), 0.001);
+        assertEquals(-0.9089, negativeVector1.normalize().getZ(), 0.001);
+    }
+
+    /**
+     * If we try to normalize a zero vector (0,0,0) the magnitude will be zero
+     * And it can not be normalized. Therefore, an IllegalStateException should be thrown
+     */
+    @org.junit.Test(expected = IllegalStateException.class)
+    public void normalizeFailure() {
+
+        this.zeroVector.normalize();
+    }
+
+    /**
+     * Creating an instance of Vector3D from the add method.
+     * And testing this new Instance with the expected values.
+     */
+    @org.junit.Test
+    public void add() {
+
+        //This will rector the new added vector
+        Vector3D newAddedVector = vector1.add(vector2);
+
+        assertEquals(0.55f, newAddedVector.getX(), 0.001);
+        assertEquals(11.25f, newAddedVector.getY(), 0.001);
+        assertEquals(10, newAddedVector.getZ(), 0.001);
+
+        //Two Other test
+        Vector3D newAddedVector2 = negativeVector1.add(otherVector);
+
+        assertEquals(-8.10f, newAddedVector2.getX(), 0.001);
+        assertEquals(-7.66f, newAddedVector2.getY(), 0.001);
+        assertEquals(-9.56f, newAddedVector2.getZ(), 0.001);
+
+    }
+
+    /**
+     * Creating an instance of Vector3D from the multiply method.
+     * And testing this new Instance with the expected values.
+     */
+    @org.junit.Test
+    public void multiply() {
+
+        double factor = -4.24f;
+
+        //This will rector the new added vector
+        Vector3D newAddedVector = vector1.multiply(factor);
+        Vector3D newAddedVector2 = negativeVector1.multiply(factor);
+
+        assertEquals(-8.4804, newAddedVector.getX(), 0.001);
+        assertEquals(-16.9604, newAddedVector.getY(), 0.001);
+        assertEquals(-21.2038, newAddedVector.getZ(), 0.001);
+
+        assertEquals(6.6144, newAddedVector2.getX(), 0.001);
+        assertEquals(50.88, newAddedVector2.getY(), 0.001);
+        assertEquals(22.3872, newAddedVector2.getZ(), 0.001);
+
+    }
+
+    /**
+     * Test the DotProduct method. The dot product of this vector and another vector.
+     * It should not change the two vectors.
+     */
+    @org.junit.Test
+    public void dotProduct() {
+
+        assertEquals(19.548f,  vector2.dotProduct(otherVector),0.001);
+        assertEquals(-111.138f,  vector2.dotProduct(negativeVector1),0.001);
+
+        //Test the other Vectors where not changed
+        assertEquals(-1.56f, negativeVector1.getX(), 0.001);
+        assertEquals(-6.54f, otherVector.getX(), 0.001);
+        assertEquals(-12, negativeVector1.getY(), 0.001);
+        assertEquals(4.34f, otherVector.getY(), 0.001);
+
+    }
+
+    /**
+     * Test the angles between both Vectors. If it's the same vector it should return 0
+     */
+    @org.junit.Test
+    public void angleBetween() {
+
+        //same vector should return 0;
+        assertEquals(0, vector1.angleBetween(vector1),0.001);
+        assertEquals(99.4001, negativeVector1.angleBetween(otherVector),0.001);
+        assertEquals(75.8195, vector2.angleBetween(otherVector),0.001);
+    }
+
+    /**
+     * Illegal State if the magnitude of either vector is zero.
+     */
+    @org.junit.Test(expected = IllegalStateException.class)
+    public void angleBetweenException() {
+
+        //If we have a magnitude of zero this must fail
+        assertEquals(75.81f, vector2.angleBetween(zeroVector),0.001);
+    }
+
+    /**
+     * Test the CrossProduct needs to create a new instance of Vector3D
+     * ande verify the initial Vectors where not modified;
+     */
+    @org.junit.Test
+    public void crossProduct() {
+
+        Vector3D crossVector = negativeVector1.crossProduct(otherVector);
+
+        assertEquals(74.2752, crossVector.getX(), 0.001);
+        assertEquals(27.8544, crossVector.getY(), 0.001);
+        assertEquals(-85.25, crossVector.getZ(), 0.001);
+
+        //Check the initial vectors where not changed
+        assertEquals(-1.56f, negativeVector1.getX(), 0.001);
+        assertEquals(-6.54f, otherVector.getX(), 0.001);
+        assertEquals(-12, negativeVector1.getY(), 0.001);
+        assertEquals(4.34f, otherVector.getY(), 0.001);
+    }
+
+    /**
+     * Test the format of the ToString method (x, y, z)
+     */
+    @org.junit.Test
+    public void testToString() {
+        assertEquals("(2.00, 4.00, 5.00)", vector1.toString());
+        assertEquals("(-1.45, 7.25, 5.00)", vector2.toString());
+        assertEquals("(-1.56, -12.00, -5.28)", negativeVector1.toString());
+        assertEquals("(-6.54, 4.34, -4.28)", otherVector.toString());
+        assertEquals("(0.00, 0.00, 0.00)", zeroVector.toString());
+    }
+}
